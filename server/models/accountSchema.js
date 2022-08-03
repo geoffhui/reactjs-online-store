f28dc77c0385de1e66f8cae539c4347c342cccf7
geoffhui <46513334+geoffhui@users.noreply.github.com>
2022-08-03 10:26:02 -0700

    created account schema

---
+++ server/models/accountSchema.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+   full_name: {
+      type: String,
+      required: true
+   },
+   email: {
+      type: String,
+      required: true,
+      unique: true
+   },
+   password: {
+      type: String,
+      required: true
+   },
+   phone_number: {
+      type: String,
+      required: false
+   },
+   restock_notification: {
+      type: Boolean,
+      required: true,
+      default: false
+   },
+   is_guest_account: {
+      type: Boolean,
+      required: true,
+      default: false
+   },
+   is_blocked: {
+      type: Boolean,
+      required: true,
+      default: false
+   },
+   shipping_address_id: {
+      type: String,
+      required: false
+   },
+   payment_info_id: {
+      type: String,
+      required: false
+   }
+})
+
+module.exports = mongoose.model('accounts', accountSchema)
