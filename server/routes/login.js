983bc3e79c88d7aa3711999e5a3e05d39b8f8d92
geoffhui <46513334+geoffhui@users.noreply.github.com>
2022-08-09 13:21:49 -0700

    fixed bug causing issue with posting to '/login' route

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const { postLogin } = require('../controllers/login');
 
-router.post('./login', postLogin)
+router.post('/login', postLogin)
 
 module.exports = router;
