59f5c90fbdca0723c1caf93533632872e0d137a3
geoffhui <46513334+geoffhui@users.noreply.github.com>
2022-08-10 11:04:54 -0700

    created second navbar for pages that don't require all functions within the first navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
    return (
       <div className='row m-auto py-3 bg-color-0059a7'>
          <div className='col-1 col-sm-1 col-md-1 col-lg-1 text-white'><FiMenu size='36px'/></div>
-         <div className='col-2 col-sm-2 col-md-2 col-lg-2 py-1 text-wrap text-white'>Online Shop</div>
+         <div className='col-2 col-sm-2 col-md-2 col-lg-2 py-1 text-wrap text-white'>Logo Here</div>
          <div className='col-7 col-sm-7 col-md-7 col-lg-7 py-1 text-center bg-light'>Search</div>
          <div className='col-2 col-sm-2 col-md-2 col-lg-2 py-1 text-center text-white'>Login / Register</div>
       </div>
---
+++ client/src/components/SecondaryNavBar.js
@@ -0,0 +1,9 @@
+const SecondaryNavBar = () => {
+   return (
+      <div className='row m-auto py-3 bg-color-0059a7'>
+         <a href="/" className='col-12 py-1 text-wrap text-white text-center'>Logo Here</a>
+      </div>
+   );
+}
+ 
+export default SecondaryNavBar;
