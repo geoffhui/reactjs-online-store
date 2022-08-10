cb1ea27c53cc77b3a992897c580e97ec272939d2
geoffhui <46513334+geoffhui@users.noreply.github.com>
2022-08-10 12:39:43 -0700

    added more data within get request /getUser route

--- server/App.js
+++ server/App.js
@@ -61,7 +61,11 @@ app.get('/getUser', verifyJWT, (req, res) => {
    res.json({
       isLoggedIn: true,
       name: req.user.name,
-      email: req.user.email
+      email: req.user.email,
+      phone_number: req.user.phone_number,
+      restock_notification: req.user.restock_notification,
+      is_guest_account: req.user.is_guest_account,
+      is_blocked: req.user.is_blocked
    })
 })
 
