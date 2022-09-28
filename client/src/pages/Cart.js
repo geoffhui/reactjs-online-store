7392cd7388f25228105a4145a81d4e367f406e84
geoffhui <46513334+geoffhui@users.noreply.github.com>
2022-09-28 13:38:09 -0700

    added total price calculation

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -3,9 +3,32 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import { useEffect } from "react";
+import { displaySalePrice } from '../functions/functions'
 
 const Cart = () => {
    const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')))
+   const [totalPrice, setTotalPrice] = useState(0)
+
+   const loadCartPrice = () => {
+      let finalPrice = 0
+      
+      cart.map(item => {
+         if (item.is_on_sale) {
+            let salePrice = displaySalePrice(item.price, item.discount_type, item.discount_value).replace('$', '')
+            let convertedSalePrice = new Number(parseFloat(salePrice)).toFixed(2)
+            finalPrice = new Number(parseFloat(finalPrice) + parseFloat(convertedSalePrice)).toFixed(2)
+         } else {
+            let convertedProductPrice = new Number(parseFloat(item.price)).toFixed(2)
+            finalPrice = new Number(parseFloat(finalPrice) + parseFloat(convertedProductPrice)).toFixed(2)
+         }
+      })
+      setTotalPrice(finalPrice)
+   }
+
+   useEffect(() => {
+      loadCartPrice()
+   }, [])
 
    return (  
       <div className="container-fluid">
@@ -35,7 +58,7 @@ const Cart = () => {
             <div className="col-2"></div>
             <div className="col-3"></div>
             <div className="col-2">Total</div>
-            <div className="col-2">$0.00</div>
+            <div className="col-2">${totalPrice}</div>
             <div className="col-2"></div>
          </div>
 
