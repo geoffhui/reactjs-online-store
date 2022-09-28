abb9b4d8c7fa3c2220f7a1bd8becdd5b8724cb78
geoffhui <46513334+geoffhui@users.noreply.github.com>
2022-09-28 11:19:50 -0700

    created cart page UI

--- client/src/components/CartProduct.js
+++ client/src/components/CartProduct.js
@@ -1,7 +1,37 @@
+import { displaySalePrice } from '../functions/functions'
+import { BsTrash } from 'react-icons/bs'
+
 const CartProduct = ({ product }) => {
    return (  
-      <div className="">
-         { product.product_name }
+      <div className='row my-5 justify-content-center text-center'>
+         <div className='col-2 '>
+            <img src={`./${product.image_url}/${product._id}_1.jpg`} alt="img" className="img-thumbnail cart-product-image" />
+         </div>
+         
+         <div className="col-3">{ product.product_name }</div>
+
+         <div className="col-2">
+            <div className='row'>
+            <div className="col-4">-</div>
+            <div className="col-4">1</div>
+            <div className="col-4">+</div>
+            </div>
+         </div>
+
+         { product.is_on_sale ? 
+            <div className="col-2">
+               <strike className='text-danger'>{ product.price }</strike>
+               <div className='text-success'>
+                  { displaySalePrice(product.price, product.discount_type, product.discount_value) }
+               </div>
+            </div>
+            : 
+            <div className='col-2'>{ product.price }</div>
+         }
+
+         <div className='col-2'>
+            <BsTrash className=''/>
+         </div>
       </div>
    );
 }
--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -13,15 +13,39 @@ const Cart = () => {
             <Navbar />
          </div>
 
-         <div className="row" key={ uuidv4() }>
-            { cart.map(item => {
+         <div className="row my-5 justify-content-center text-center" key={ uuidv4() }>
+            <div className="col-2">Product</div>
+            <div className="col-3">Name</div>
+            <div className="col-2">Quantity</div>
+            <div className="col-2">Price</div>
+            <div className="col-2"></div>
+         </div>
+
+         { cart.map(item => {
                return (
                   <div key={ uuidv4() }>
                      <CartProduct product={ item }/>
                   </div>
                )
-            }) }
+         }) }
+
+         <hr></hr>
+
+         <div className="row my-3 justify-content-center text-center" key={ uuidv4() }>
+            <div className="col-2"></div>
+            <div className="col-3"></div>
+            <div className="col-2">Total</div>
+            <div className="col-2">$0.00</div>
+            <div className="col-2"></div>
          </div>
+
+         <div className="row my-5">
+            <div className="col-2"></div>
+            <div className="col-3"></div>
+            <div className="col-3"></div>
+            <button className="col-1 btn btn-primary">Checkout</button>
+         </div>
+         
 
          <div className='row'>
             <Footer />
